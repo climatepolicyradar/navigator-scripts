465cde6a607a36db91d36598244c2f217c847a0c
diversemix <diversemix@gmail.com>
2023-01-30 13:51:38 +0000

    only start backend

--- nav-local-as-prod.sh
+++ nav-local-as-prod.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+set -euo pipefail
+
 echo Script needs: 
 echo "    - to be run in the root of the backed repo."
 echo "    - to have a prod-database volume"
 
-docker-compose down
+make stop
+docker rm navigator-backend_backend_1 navigator-backend_backend_db_1 || echo Skipped
 docker volume rm navigator-backend_db-data-backend 
 dkr-copy-volume.sh prod-database navigator-backend_db-data-backend 
-make start
+make start_backendonly
+    
