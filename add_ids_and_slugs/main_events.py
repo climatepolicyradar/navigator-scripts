6bdbd11764d44c2bb9ad7943117b11c10d7d495b
Joel Wright <joel@climatepolicyradar.org>
2023-03-24 16:47:04 +0000

    More cleanup

--- add_ids_and_slugs/main_events.py
+++ add_ids_and_slugs/main_events.py
@@ -10,7 +10,7 @@ import csv
 import sys
 from collections import defaultdict
 from pathlib import Path
-from typing import Any
+from typing import Any, Mapping
 
 REQUIRED_DFC_COLUMNS = [
     "ID",
@@ -62,6 +62,11 @@ REQUIRED_EVENTS_COLUMNS = [
     "Description",
     "Date",
     "Url",
+]
+EXTRA_EVENTS_COLUMNS = [
+    "CPR Event ID",
+    "CPR Family ID",
+    "Event Status",
 ]
 
 
@@ -124,6 +129,7 @@ def _read_existing_dfc_data(
             # If CPR Family ID is already set, look for existing info & validate it
             if cpr_family_id := row.get("CPR Family ID", ""):
                 cpr_family_id = cpr_family_id.strip()
+                action_id = row.get("ID", "")
                 if cpr_family_info := existing_family_info.get(cpr_family_id):
                     # We've seen this family before, so make sure the values we
                     # already have are consistent
@@ -156,36 +162,104 @@ def _read_existing_dfc_data(
                         "Family name": row.get("Family name", "").strip(),
                         "CPR Family Slug": cpr_family_slug,
                         "Document IDs": [cpr_document_id],
+                        "Action ID": action_id,
                     }
 
-                if action_id := row.get("ID", ""):
+                if action_id:
                     action_id_to_family_id[action_id].add(cpr_family_id)
 
         if errors:
             sys.exit(10)
 
 
-def _read_existing_event_data(
+def _process_event_data(
     event_csv_file_path: Path,
     ambiguous_event_info: dict[str, list[dict[str, Any]]],
-    action_id_to_family_id: dict[str, set[str]],
-) -> None:
+    action_id_to_family_id: Mapping[str, set[str]],
+    existing_family_info: dict[str, dict[str, Any]],
+) -> list[dict[str, str]]:
     # First pass to load existing IDs/Slugs
+    families_passed_approved: set[str] = set()
+    families_with_events: set[str] = set()
+    family_events = []
     with open(event_csv_file_path) as event_csv_file:
         event_reader = csv.DictReader(event_csv_file)
-        print(event_reader.fieldnames)
         assert set(REQUIRED_EVENTS_COLUMNS).issubset(set(event_reader.fieldnames or []))
         row_count = 0
-        actions_identified = 0
 
         for row in event_reader:
             row_count += 1
             if action_id := row.get("Eventable Id", ""):
+                event_source_type = row.get("Eventable type", "").strip()
+                if not event_source_type or event_source_type != "Legislation":
+                    continue
                 if action_id in action_id_to_family_id:
-                    actions_identified += 1
+                    event_type = row.get("Event type", "")
+                    if event_type.strip():
+                        families_with_events = families_with_events | set(
+                            action_id_to_family_id[action_id]
+                        )
+                    if event_type.strip() == "Passed/Approved":
+                        families_passed_approved = families_passed_approved | set(
+                            action_id_to_family_id[action_id]
+                        )
+
+                    event_id = row.get("Id", "")
+                    event_ids = [
+                        f"CCLW.legislation_event.{event_id}.{i}"
+                        for i, _ in enumerate(action_id_to_family_id[action_id])
+                    ]
+
+                    event_status = "OK"
                     if len(action_id_to_family_id[action_id]) > 1:
                         ambiguous_event_info[action_id].append(row)
-        print(f"Identified {actions_identified} actions related to known documents")
+                        event_status = "DUPLICATED"
+
+                    family_events.extend(
+                        [
+                            {
+                                **row,
+                                **{
+                                    "CPR Event ID": event_id,
+                                    "CPR Family ID": family_id,
+                                    "Event Status": event_status,
+                                },
+                            }
+                            for (event_id, family_id) in zip(
+                                event_ids, action_id_to_family_id[action_id]
+                            )
+                        ]
+                    )
+
+        families_without_events = [
+            {
+                "Family ID": family,
+                "CCLW Action ID": existing_family_info[family]["Action ID"],
+            }
+            for family in set(existing_family_info.keys()) - families_with_events
+        ]
+        print(f"Found {len(families_without_events)} families without events:")
+        for f_a in families_without_events:
+            print(
+                f"Family without any event: {f_a['Family ID']} (CCLW Action {f_a['CCLW Action ID']})"
+            )
+
+        families_without_pa = [
+            {
+                "Family ID": family,
+                "CCLW Action ID": existing_family_info[family]["Action ID"],
+            }
+            for family in families_with_events - families_passed_approved
+        ]
+        print(
+            f"Found {len(families_without_pa)} families without Passed/Approved event:"
+        )
+        for f_a in families_without_pa:
+            print(
+                f"Family without passed/approved event: {f_a['Family ID']} (CCLW Action {f_a['CCLW Action ID']})"
+            )
+
+    return family_events
 
 
 def _process_csvs(dfc_csv_file_path: Path, events_csv_file_path: Path):
@@ -204,44 +278,36 @@ def _process_csvs(dfc_csv_file_path: Path, events_csv_file_path: Path):
 
     ambiguous_event_info = defaultdict(list)
 
-    _read_existing_event_data(
+    family_events = _process_event_data(
         events_csv_file_path,
         ambiguous_event_info,
         action_id_to_family_id,
+        existing_family_info,
     )
 
     print(f"Identified {len(existing_family_info)} families")
-    return ambiguous_event_info, action_id_to_family_id
+    return family_events
 
 
 def _write_file(
-    ambiguous_event_info: dict[str, list[dict[str, Any]]],
-    action_id_to_family_id: dict[str, set[str]],
+    family_events: list[dict[str, str]],
     output_path: Path,
 ) -> None:
-    csv_output_fieldnames = REQUIRED_EVENTS_COLUMNS + ["Action ID", "Family IDs"]
+    csv_output_fieldnames = REQUIRED_EVENTS_COLUMNS + EXTRA_EVENTS_COLUMNS
     with open(output_path, "w") as out_csv:
         writer = csv.DictWriter(out_csv, fieldnames=csv_output_fieldnames)
         writer.writeheader()
-        for action_id, events in ambiguous_event_info.items():
-            for row in events:
-                row.update(
-                    {
-                        "Action ID": action_id,
-                        "Family IDs": ";".join(action_id_to_family_id[action_id]),
-                    }
-                )
-                writer.writerow(row)
-            writer.writerow({})
+        for processed_row in family_events:
+            writer.writerow(processed_row)
 
 
 def main():
     dfc_csv_file_path = Path(sys.argv[1]).absolute()
     events_csv_file_path = Path(sys.argv[2]).absolute()
-    action_events, action_families = _process_csvs(
+    family_events = _process_csvs(
         dfc_csv_file_path, events_csv_file_path
     )
-    _write_file(action_events, action_families, Path(f"{sys.argv[1]}_grouped_events"))
+    _write_file(family_events, Path(f"{sys.argv[2]}_processed"))
     print("DONE")
 
 
