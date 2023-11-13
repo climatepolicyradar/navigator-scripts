a7c1cf108a0fb32c7cae8547cff5715a48bab854
diversemix <diversemix@gmail.com>
2023-11-13 15:13:12 +0000

    User RDS database in vars

--- nav-stack-connect.sh
+++ nav-stack-connect.sh
@@ -22,21 +22,22 @@ pulumi stack output > ${OUTPUT}
 DB_USER=$(pulumi config get backend:rds_username)
 DB_PASS=$(pulumi config get backend:rds_password)
 RDS_ADDRESS=$(pulumi config get backend:rds_address)
+RDS_DB=$(pulumi config get backend:rds_database)
 BASTION_TARGET=$(grep bastion.id ${OUTPUT} | cut -d ' ' -f7 )
 echo
 echo "🔨 Building db connection string..."
 
 # These values are exported so you can use a script in your current shell
 VAR_FILE=~/.aws/${AWS_PROFILE}_vars.sh
-echo export DATABASE_URL=postgresql://${DB_USER}:${DB_PASS}@localhost:5434/navigator > ${VAR_FILE}
-echo export BACKEND_DATABASE_URL=postgresql://${DB_USER}:${DB_PASS}@localhost:5434/navigator >> ${VAR_FILE}
+echo export DATABASE_URL=postgresql://${DB_USER}:${DB_PASS}@localhost:5434/${RDS_DB} > ${VAR_FILE}
+echo export BACKEND_DATABASE_URL=postgresql://${DB_USER}:${DB_PASS}@localhost:5434/${RDS_DB} >> ${VAR_FILE}
 echo export SECRET_KEY=$(pulumi config get backend:backend_secret_key) >> ${VAR_FILE}
 echo export SUPERUSER_EMAIL=$(pulumi config get backend:superuser_email) >> ${VAR_FILE}
 echo export SUPERUSER_PASSWORD=$(pulumi config get backend:superuser_password) >> ${VAR_FILE}
 echo export PGUSER=$DB_USER >> ${VAR_FILE}
 echo export PGPASSWORD=$DB_PASS >> ${VAR_FILE}
 echo export PGPORT=5434 >> ${VAR_FILE}
-echo export PGDATABASE=navigator >> ${VAR_FILE}
+echo export PGDATABASE=${RDS_DB} >> ${VAR_FILE}
 chmod 0600 ${VAR_FILE}
 
 echo
